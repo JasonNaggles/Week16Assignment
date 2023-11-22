0da52016d8fa6643377d345dd9db44214718c3de
JasonNaggles <JasonNaggles.verizon.net>
2023-11-21 21:03:04 -0500

    first commit

--- src/Components/CatForm.js
+++ src/Components/CatForm.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Link, useHistory } from "react-router-dom";
-import { catOptions } from "../options";
+import { Link } from "react-router-dom";
 
 export default function CatForm() {
   const [catData, setCatData] = useState(null);
   const fetchData = () => {
-    axios.get("https://api.thecatapi.com/v1/images/search?limit=10",
-    catOptions
+    axios.get("https://api.thecatapi.com/v1/images/search",
     ).then((response) => setCatData(response.data))
     .catch((error) => console.error("error during fetching"));
   };
@@ -16,42 +13,7 @@ export default function CatForm() {
   }, []);
   
   console.log(catData);
-  const history = useHistory();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setCatData({
-      ...catData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Define the URL for your mock API
-    const apiUrl = "https://api.thecatapi.com/v1/images/search?limit=10"; // Replace with your API URL
-
-    fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(catData),
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          // Successfully created the cat, so redirect to the cat list page
-          history.push("/cats");
-        } else {
-          // Handle error cases here
-        }
-      })
-      .catch((error) => {
-        // Handle any network or request error
-        console.error("Error:", error);
-      });
-  };
 
   return (
     <div className="container">
--- src/options.js
+++
@@ -1,9 +0,0 @@
-import { apiKey } from "./env";
-
-export const catOptions = {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/json",
-        "x-api-key": apiKey,
-    },
-};
