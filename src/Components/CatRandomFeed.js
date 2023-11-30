12224d82596ff1973b80f7016141e104fb4fcdfc
JasonNaggles <JasonNaggles.verizon.net>
2023-11-30 01:00:28 -0500

    first commit

--- src/Components/CatRandomFeed.js
+++ src/Components/CatRandomFeed.js
@@ -1,3 +1,4 @@
+import { Grid } from "@mui/material";
 import React from "react";
 
 const Feed = () => {
@@ -22,4 +23,9 @@ const Feed = () => {
     }       
 }
 
+return (
+    <Grid container columns={{xs:4 , sm:8, md:12}}>
+    <h1>CatRandomFeed</h1>
+    </Grid>
+);
 }
