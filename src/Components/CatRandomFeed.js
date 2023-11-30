6eaea01f3a431fead5f65b6fce3285d7a78e492d
JasonNaggles <JasonNaggles.verizon.net>
2023-11-30 00:54:21 -0500

    first commit

--- src/Components/CatRandomFeed.js
+++ src/Components/CatRandomFeed.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Feed = () => {
     
-    const [images, setImages] = useState([]);
+    const [images, setImages] = React.useState([]);
 
     async function refreshImages()
     {
         setImages([])
 
         try {
-            const response = await fetch("https://api.thecatapi.com/v1/images/search");
+            const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=12",{
+                headers:{
+                    "x-api-key": process.env.REACT_APP_CAT_API_KEY
+                }
+            });
             const json = await response.json();
             setImages(json);
         }
