0c83fc389cadd202a6db5437bff6eb29cba3ef4f
JasonNaggles <JasonNaggles.verizon.net>
2023-12-03 23:14:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
-import Dog from "./Components/Dogs";
+import Dogs from "./Components/Dogs";
 import DogDetail from "./Components/DogDetail";
 import DogEditForm from "./Components/DogEditForm";
 import DogDeleteForm from "./Components/DogDeleteForm";
@@ -20,7 +20,7 @@ export default function App() {
         <Header />
         <Switch>
           <Route path="/" component={Home} exact />
-          <Route path="/dogs/add" component={Dog} />
+          <Route path="/dogs/add" component={Dogs} />
           <Route path="/dogs/dog" component={DogDetail} />
           <Route path="/dogs/edit" component={DogEditForm} />
           <Route path="/dogs/delete" component={DogDeleteForm} />
--- src/Components/Dogs.js
+++ src/Components/Dogs.js
@@ -1,33 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
-export default function Dog() {
+export default function Dogs() {
   
-  const [dogs, setDogs ] = useState([null]);
+  const [dogs, setDogs ] = useState([]);
+  const [error, setError] = useState(false);
   const history = useHistory();
   
   useEffect(() => {
     const getDogs = async () => {
+      try {
         const response = await axios.get('https://dog.ceo/api/breeds/image/random/4',
       );
         console.log(response);
         setDogs(response.data.message);
+      } catch (error) {
+        setError(error);
+      }
   };
     getDogs();
   }, []);
 
 
   return (
+    <>
     <div className="container">
-      <h1>Dog </h1>
-      <img alt="picture of a dog" src={dogs} onClick={() => history.push("/")} />
-      <img alt="picture of a dog" src={dogs} onClick={() => history.push("/")} />
-      <img alt="picture of a dog" src={dogs} onClick={() => history.push("/")} />
-      <img alt="picture of a dog" src={dogs} onClick={() => history.push("/")} />
+      <h1>Dogs </h1>
+      </div>
+      {error ? (
+        <div>
+          We are sorry, but an unexpected error ocurred{" "}
+        </div>  
+      ) : null}
+      {dogs && 
+      dogs.map((dog) => {
+          return (
+            <div key ={dog}>
+      
+      <img alt="picture of a dog" src={dog} onClick={() => history.push("/")} />
     </div>
-
-
+          );
+      })}
+       </>
   );
-
-  };
-
+}
