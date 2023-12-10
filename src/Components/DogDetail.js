85888537fb254de8c45a601ab89ff6d9bd6f3cf8
JasonNaggles <JasonNaggles.verizon.net>
2023-12-09 23:24:47 -0500

    first commit

--- src/Components/DogDetail.js
+++ src/Components/DogDetail.js
@@ -1,44 +1,53 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link, useHistory } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 export default function DogDetail() {
-  const { dogId } = useParams(); // Get the dog from the URL
-  const history = useHistory(); 
-  const [ dog, setDogs ] = useState(null);
+  const { name } = useParams(); // Get the dog from the URL 
+  const [ dog, setDog ] = useState(null);
   
   
   useEffect(() => {
   // Assuming you fetch the dog data using an API call or from a database
   const fetchDogData = async () => {
     try {
-      // Make an API call or fetch data based on the dogId parameter
-      const response = await fetch(`https://dog.ceo/api/breeds/image/random/4/${dogId}`);
-      console.log(response);
-        setDogs(response.data.message);
+      // Make an API call or fetch data based on the name parameter
+      const response = await fetch(`https://api.thedogapi.com/v1/breeds/search?q=${name}`);
+      const data = response.json();
+        setDog(data);
+        console.log(data);
       } catch (error) {
-        setError(true);
+        console.error(error);
       }
   };
 
   fetchDogData();
-}, [dogId]);
+}, [name]);
 
-if (!dog) {
-  return <div>Loading...</div>; // Show a loading message while fetching data
-}
 
   return (
-    <div className="container">
-      <h2>Dog Details</h2>
-      <div>
-        <h3>{dog.name}</h3>
-        <p>Breed: {dog.breed}</p>
-        <p>Age: {dog.age} years</p>
-        <p>Description: {dog.description}</p>
-        <Link to={`/dogs/edit/${dogId}`}>Edit Dog</Link>
-        <Link to={`/dogs/delete/${dogId}`}>Delete Dog</Link>
-        <Link to="/dogs">Back to Dog List</Link>
-      </div>
-    </div>
+    <>
+    <section className="max-w-5xl mx-auto">
+      {dog.map((item) => (
+        <div key={item.id}>
+          <article>
+            <img src={`https://cdn2.thedogapi.com/images/${item.reference_image_id}.jpg`} alt={dog.name} />
+          </article>
+          <article>
+            <h1>{item.name}</h1>
+            {item.description && <p>{item.description}</p>}
+            <ul>
+              <li>Bred For: {item.bred_for}</li>
+              <li>Height: {item.height.metric} cm</li>
+              <li>Weight: {item.weight.metric} kgs</li>
+              <li>Breed Group: {item.breed_group}</li>
+              <li>Lifespan: {item.life_span}</li>
+              <li>Temperament: {item.temperament}</li>
+            </ul>
+          </article>
+        </div>
+      ))}
+      <h1 classname="text-white text-3xl">{name}</h1>
+    </section>
+    </>
   );
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 export default function Home() {
 
   const [dogs, setDogs ] = useState([]);
@@ -25,7 +26,7 @@ export default function Home() {
       <h1 className="flex items-center justify-center text-slate-800 text-center px-5 text-3xl h-screen font-bold uppercase">Loading...</h1>
     ) : (
       <>
-      <section className="p-8 max-w-6xl mx-auto">
+      <section className="p-8 max-w-7xl mx-auto">
         <div className="text-center">
       <h1 className="flex items-center justify-center text-white text-center px-5 text-3xl font-bold lg:text-5xl">The Dog App</h1>
       <p className="my-8 text-white">This application is powered by{" "} <a href="https://thedogapi.com" className="text-indigo-600 underline active:text-orange-400">The Dog API</a></p>
@@ -34,13 +35,15 @@ export default function Home() {
         className="py-2 px-4 rounded shadow w-full bg-slate-400 text-white placeholder-white" />
       </form>
       </div>
-      <div>
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 my-10 lg:my-20">
       {dogs.map((dog) => (
-        <article key={dog.id}>
-          <img src={`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`} alt={dog.name} />
-          <h3>{dog.name}</h3>
-          <p>Bred For: {dog.bred_for}</p>
+        <Link to={`/${dog.name}`} key={dog.id}>
+        <article className="bg-slate-700 p-4 rounded">
+          <img src={`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`} alt={dog.name} loading="lazy" className="rounded md:h-72 w-full"/>
+          <h3 className="text-white text-lg font-bold mt-4">{dog.name}</h3>
+          <p className="text-slate-400">Bred For: {dog.bred_for}</p>
         </article>
+        </Link>
       ))}
       </div>
       </section>
