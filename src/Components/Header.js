1f033d437179a657b87f0b1dbfe74f655006cb30
JasonNaggles <JasonNaggles.verizon.net>
2023-12-24 20:18:25 -0500

    first commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="bg-gray-100">
-      <nav className="container mx-auto flex items-center justify-between py-4">
+      <nav className="container mx-auto flex items-center justify-center py-4">
         <button
           className="block lg:hidden px-3 py-2 border rounded text-gray-600 border-gray-600 hover:text-gray-900 hover:border-gray-900"
           type="button"
