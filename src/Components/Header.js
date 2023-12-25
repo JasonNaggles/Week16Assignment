e650be7f309619042a24fdb198c0a6f00f88d2cd
JasonNaggles <JasonNaggles.verizon.net>
2023-12-24 20:11:32 -0500

    first commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,59 +3,55 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="header">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/add" className="nav-link">
-                  Dogs
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/:name" className="nav-link">
-                  DogDetail
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/edit" className="nav-link">
-                  DogEditForm
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/delete" className="nav-link">
-                  DogDeleteForm
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/notfound" className="nav-link">
-                  NotFound
-                </Link>
-              </li>
-            </ul>
-          </div>
+    <header className="bg-gray-100">
+      <nav className="container mx-auto flex items-center justify-between py-4">
+        <button
+          className="block lg:hidden px-3 py-2 border rounded text-gray-600 border-gray-600 hover:text-gray-900 hover:border-gray-900"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="hidden lg:block">
+          <ul className="flex space-x-4">
+            <li className="nav-item">
+              <Link to="/" className="text-gray-600 hover:text-gray-900">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/add" className="text-gray-600 hover:text-gray-900">
+                Dogs
+              </Link>
+            </li>
+            {/* Replace `/:name` with a specific route path */}
+            <li className="nav-item">
+              <Link to="/details" className="text-gray-600 hover:text-gray-900">
+                DogDetail
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/edit" className="text-gray-600 hover:text-gray-900">
+                DogEditForm
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/delete" className="text-gray-600 hover:text-gray-900">
+                DogDeleteForm
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/notfound" className="text-gray-600 hover:text-gray-900">
+                NotFound
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </header>
   );
 }
-
-
-
