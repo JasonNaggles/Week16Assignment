f47d98956302e3dcb79ef0bc9c73ac0adb5bb84a
JasonNaggles <JasonNaggles.verizon.net>
2024-01-02 22:45:32 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import Home from "./Components/Home";
-import DogAdd from "./Components/DogAdd";
-import DogDetail from "./Components/DogDetail";
-import DogEditForm from "./Components/DogEditForm";
-import DogDeleteForm from "./Components/DogDeleteForm";
-import NotFound from "./Components/NotFound";
-
+import FriendsDetail from "./Components/FriendsDetail";
 
 
 export default function App() {
@@ -17,16 +13,20 @@ export default function App() {
   return (
   <BrowserRouter>
       <div className="App">
-        <Header />
+        <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Nav>
+          <Nav.Link href="/Home">Home</Nav.Link>
+          <Nav.Link href="/FriendsDetail">FriendsDetail</Nav.Link>
+          </Nav>
+        </Container>
+        </Navbar>
         <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/add" component={DogAdd} />
-          <Route path="/detail" component={DogDetail} />
-          <Route path="/edit" component={DogEditForm} />
-          <Route path="/delete" component={DogDeleteForm} />
-          <Route path="/notfound" component={NotFound} />
+          <Route path="/FriendsDetail"></Route>
+          <FriendsDetail />
+          <Route path="/"></Route>
+          <Home />
         </Switch>
-        <Footer />
       </div>
   </BrowserRouter>
   );
