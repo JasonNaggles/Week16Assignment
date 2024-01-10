ea97feb030f313f18c7836dd40a37e0e21d0c029
JasonNaggles <JasonNaggles.verizon.net>
2024-01-09 20:31:04 -0500

    first commit

--- src/Components/FriendsDetailForm.js
+++ src/Components/FriendsDetailForm.js
@@ -1,2 +1,14 @@
 import React from "react";
 import FriendsDetailCRUD from "./Components/FriendsDetailCRUD";
+
+//Making a function to export
+
+export default function FriendsDatailForm() {
+    return (
+        <div>
+            <h2>Enter the Friends Data</h2>
+            <FriendsDetailCRUD />
+        </div>
+    )
+
+}
