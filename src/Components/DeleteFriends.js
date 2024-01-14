f001ff552fa3dad8d0edf36fe073c8a0b79853b8
JasonNaggles <JasonNaggles.verizon.net>
2024-01-13 20:15:35 -0500

    first commit

--- src/Components/DeleteFriends.js
+++ src/Components/DeleteFriends.js
@@ -1,11 +1,12 @@
 import React from "react";
-export default function DeleteFriends( getFriends ) {
+export default function DeleteFriends({ id, onDelete, getFriends }) {
 
     // MOCK API URL used to update MockAPI
 const MOCK_API_URL = "https://650fc3383ce5d181df5ca880.mockapi.io/Friends";
 
     // Function to delete a friend by ID
- function deleteFriends(id) {
+ function deleteFriends() {
+    onDelete(id)
     fetch(`${MOCK_API_URL}/${id}`, {
       method: 'DELETE',
     }).then(() => getFriends()) // Fetch friends data again after deletion
