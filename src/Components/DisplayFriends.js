11d861cd9d0a3022da4b15a689e134becbef7443
JasonNaggles <JasonNaggles.verizon.net>
2024-02-02 23:10:06 -0500

    first commit

--- src/Components/DisplayFriends.js
+++ src/Components/DisplayFriends.js
@@ -40,7 +40,7 @@ export default function DisplayFriends () {
         <div className="displayFriend text-center">
             {friends.map((friend, index) => (
                 <div className="mapContainer d-inline-flex flex-row p-2 m-2 justify-content-around" key={index}>
-                <UpdateFriends id ={friend.id} getFriends={getFriends} onUpdate={UpdateFriends}/>      
+                <UpdateFriends id={friend.id} getFriends={getFriends} onUpdate={UpdateFriends}/>      
                 </div>
             ))}
         </div>
