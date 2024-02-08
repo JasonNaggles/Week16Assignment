c6f0e25ec45ea121424dfdd27ccb35b84f65c034
JasonNaggles <JasonNaggles.verizon.net>
2024-02-07 20:39:44 -0500

    first commit

--- src/Components/DisplayFriends.js
+++ src/Components/DisplayFriends.js
@@ -2,9 +2,6 @@ import { useEffect, useState } from "react";
 import UpdateFriends from "./UpdateFriends";
 import "./DisplayFriends.css"
 
-
-
-
 export default function DisplayFriends () {
     // This is my URL for Mock API
     const MOCK_API_URL = "https://650fc3383ce5d181df5ca880.mockapi.io/Friends";
@@ -24,6 +21,43 @@ export default function DisplayFriends () {
     getFriends()
   }, [])
 
+  // Function to delete a friend by ID
+  function deleteFriends(id) {
+    fetch(`${MOCK_API_URL}/${id}`, {
+      method: "DELETE",
+    }).then(() => getFriends()); // Fetch friends data again after deletion
+  }
+
+  // Function to add a new friend
+  function postNewFriends(e) {
+    e.preventDefault();
+    fetch(MOCK_API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firstName: newFirstName,
+        lastName: newLastName,
+      }),
+    }).then(() => getFriends()); // Fetch friends data again after adding a new friend
+  }
+
+  // Function to update a friend's information
+  function updateFriends(e, friendsObject) {
+    e.preventDefault();
+    let updatedFriendsObject = {
+      firstName: updatedFirstName,
+      lastName: updatedLastName,
+    };
+    fetch(`${MOCK_API_URL}/${friendsObject.id}`, {
+      method: "PUT",
+      body: JSON.stringify(updatedFriendsObject),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => getFriends()); // Fetch friends data again after updating a friend
+  }
+
+  console.log(friends);
 
   return (
     <div style={
@@ -38,11 +72,15 @@ export default function DisplayFriends () {
       }>
     <div>
         <div className="displayFriend text-center">
-            {friends.map((friend, index) => (
-                <div className="mapContainer d-inline-flex flex-row p-2 m-2 justify-content-around" key={index}>
-                <UpdateFriends id={friend.id} getFriends={getFriends} onUpdate={UpdateFriends}/>      
-                </div>
-            ))}
+              <div className="mapContainer d-inline-flex flex-row p-2 m-2 justify-content-around">
+              <UpdateFriends friends={friends}
+              getFriends={getFriends}
+              deleteFriends={deleteFriends}
+              postNewFriends={postNewFriends}
+              updateFriends={updateFriends}
+              />      
+              </div>
+            
         </div>
     </div>
     </div>
